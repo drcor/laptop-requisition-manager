942ed04c38bb22ee13c85f44289834796a825a91
alexandreleyfigueiredo <leyzynhu13@gmail.com>
2022-12-15 08:23:34 +0000

    Add menu function & implement in main function

--- main.c
+++ main.c
@@ -1,8 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <locale.h>
+#include <ctype.h>
+#include <string.h>
+
+int menu();
 
 int main()
 {
-    printf("Hello world!\n");
-    return 0;
+    setlocale(LC_ALL, "Portuguese");
+    int opcao;
+
+    opcao = menu();
+    do{
+        switch(opcao){
+            case 0:
+                break;
+        }
+    }while(opcao != 0);
+    
+}
+
+int menu(){
+    int opcao;
+    printf("---------------------------------------------------\n");
+    printf("                      MENU                      \n");
+    printf("---------------------------------------------------\n");
+    printf("(C)      Teste");
+    printf("\n(C)      Teste");
+    printf("\n(C)      Teste");
+    printf("\n(C)      Teste");
+    printf("\n\n\n");
+    printf("\u2554\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2557\n");
+    printf("\u2551                MENU                 \u2551\n");
+    printf("\u2560\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2563\n");
+    printf("\u2551  [1] Teste                          \u2551\n");
+    printf("\u2551  [2] Teste                          \u2551\n");
+    printf("\u2551  [3] Teste                          \u2551\n");
+    printf("\u2551  [4] Teste                          \u2551\n");
+    printf("\u2551  [0] Sair                           \u2551\n");
+    printf("\u255A\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u255D\n");
+    scanf("%d", &opcao);
+   
+    return opcao;
 }
