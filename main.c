4ab8bc34097a879ccaa7bbaf1bf363352a9c1f9b
drcor <53583855+drcor@users.noreply.github.com>
2022-12-21 12:46:25 +0000

    Update visualization of special chars

--- main.c
+++ main.c
@@ -13,122 +13,122 @@ void menuDados(int*);
 
 
 int main(void){
-    // Configure support for UTF-16 characters
+	// Configure support for UTF-16 characters
 #if _WIN32
 	_setmode(_fileno(stdout), _O_U16TEXT);
 #endif	// _WIN32
-    setlocale(LC_CTYPE, "");
+	setlocale(LC_CTYPE, "");
 
-    int opcao;
-    
-    do { 
-        opcao = menu();
-        switch (opcao) {
-            case 0:
-                break;
-            case 1:
-                menuPortateis(&opcao);
-                break;
-            case 2:
-                menuRequisicoes(&opcao);
-                break;
-            case 3:
-                menuAvarias(&opcao);
-                break;
-            case 4:
-                menuDados(&opcao);
-                break;
-       }
-    } while(opcao != 0);
-    
-    return 0;
+	int opcao;
+	
+	do { 
+		opcao = menu();
+		switch (opcao) {
+			case 0:
+				break;
+			case 1:
+				menuPortateis(&opcao);
+				break;
+			case 2:
+				menuRequisicoes(&opcao);
+				break;
+			case 3:
+				menuAvarias(&opcao);
+				break;
+			case 4:
+				menuDados(&opcao);
+				break;
+	   }
+	} while(opcao != 0);
+	
+	return 0;
 }
 
 int menu(void) {
-    int opcao;
-            
-    // This is the main menu
-    wprintf(L"\u2554\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2557\n");
-    wprintf(L"\u2551                MENU                 \u2551\n");
-    wprintf(L"\u2560\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2563\n");
-    wprintf(L"\u2551  [1] Portáteis                      \u2551\n");
-    wprintf(L"\u2551  [2] Requisições                    \u2551\n");
-    wprintf(L"\u2551  [3] Avarias                        \u2551\n");
-    wprintf(L"\u2551  [4] Dados Estatísticos             \u2551\n");
-    wprintf(L"\u2551  [0] Sair                           \u2551\n");
-    wprintf(L"\u255A\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u255D\n");
-    opcao = lerInteiro("Opcão: ", 0, 4);
+	int opcao;
+			
+	// This is the main menu
+	wprintf(L"╔═════════════════════════════════════╗\n");
+	wprintf(L"║                MENU                 ║\n");
+	wprintf(L"╠═════════════════════════════════════╣\n");
+	wprintf(L"║  [1] Portáteis                      ║\n");
+	wprintf(L"║  [2] Requisições                    ║\n");
+	wprintf(L"║  [3] Avarias                        ║\n");
+	wprintf(L"║  [4] Dados Estatísticos             ║\n");
+	wprintf(L"║  [0] Sair                           ║\n");
+	wprintf(L"╚═════════════════════════════════════╝\n");
+	opcao = lerInteiro("Opcão: ", 0, 4);
 
-    return opcao;  
+	return opcao;  
 }
 
 void menuPortateis(int *op) {
-    int opcao2;
-    // If he chooses laptops this shows up
-    wprintf(L"\u2554\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2557\n");
-    wprintf(L"\u2551                Portáteis                 \u2551\n");
-    wprintf(L"\u2560\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2563\n");                
-    wprintf(L"\u2551  [1] Inserir Portáteis                   \u2551\n");
-    wprintf(L"\u2551  [2] Listar Portáteis                    \u2551\n");
-    wprintf(L"\u2551  [3] Alterar Localização do Portátil     \u2551\n");
-    wprintf(L"\u2551  [0] Anterior                            \u2551\n");
-    wprintf(L"\u255A\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u255D\n");
-    opcao2 = lerInteiro("Opcão", 0, 3);
+	int opcao2;
+	// If he chooses laptops this shows up
+	wprintf(L"╔══════════════════════════════════════════╗\n");
+	wprintf(L"║                Portáteis                 ║\n");
+	wprintf(L"╠══════════════════════════════════════════╣\n");                
+	wprintf(L"║  [1] Inserir Portáteis                   ║\n");
+	wprintf(L"║  [2] Listar Portáteis                    ║\n");
+	wprintf(L"║  [3] Alterar Localização do Portátil     ║\n");
+	wprintf(L"║  [0] Anterior                            ║\n");
+	wprintf(L"╚══════════════════════════════════════════╝\n");
+	opcao2 = lerInteiro("Opcão", 0, 3);
 
-    switch(opcao2) {
-        case 1:
-            wprintf(L"\nteste\n\n");
-            *op = lerInteiro("Deseja continuar no programa? Não/Sim", 0, 1);  // Asks if the user wants to coninue the program               
-            break;
-        case 0:
-            wprintf(L"\n\nteste 0000\n");
-            break;
-    }
+	switch(opcao2) {
+		case 1:
+			wprintf(L"\nteste\n\n");
+			*op = lerInteiro("Deseja continuar no programa? Não/Sim", 0, 1);  // Asks if the user wants to coninue the program               
+			break;
+		case 0:
+			wprintf(L"\n\nteste 0000\n");
+			break;
+	}
 }
 
 void menuRequisicoes(int *op) {
-    int opcao2;
-    wprintf(L"\u2554\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2557\n");
-    wprintf(L"\u2551                 Requisições              \u2551\n");
-    wprintf(L"\u2560\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2563\n");
-    wprintf(L"\u2551  [1] Requisitar Portátil                 \u2551\n");
-    wprintf(L"\u2551  [2] Listar Requisições                  \u2551\n");
-    wprintf(L"\u2551  [3] Mostrar Requisição                  \u2551\n");
-    wprintf(L"\u2551  [4] Devolução de Portátil Requisitado   \u2551\n");
-    wprintf(L"\u2551  [5] Renovar Requisição                  \u2551\n");
-    wprintf(L"\u2551  [0] Anterior                            \u2551\n");
-    wprintf(L"\u255A\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u255D\n");
-    opcao2 = lerInteiro("Opcão", 0, 5);
+	int opcao2;
+	wprintf(L"╔══════════════════════════════════════════╗\n");
+	wprintf(L"║                 Requisições              ║\n");
+	wprintf(L"╠══════════════════════════════════════════╣\n");
+	wprintf(L"║  [1] Requisitar Portátil                 ║\n");
+	wprintf(L"║  [2] Listar Requisições                  ║\n");
+	wprintf(L"║  [3] Mostrar Requisição                  ║\n");
+	wprintf(L"║  [4] Devolução de Portátil Requisitado   ║\n");
+	wprintf(L"║  [5] Renovar Requisição                  ║\n");
+	wprintf(L"║  [0] Anterior                            ║\n");
+	wprintf(L"╚══════════════════════════════════════════╝\n");
+	opcao2 = lerInteiro("Opcão", 0, 5);
 
-    switch(opcao2) {
-        case 1:
-            wprintf(L"\nteste\n\n");
-            *op = lerInteiro("Deseja continuar no programa? Não/Sim", 0, 1);  // Asks if the user wants to coninue the program               
-            break;
-        case 0:
-            break;
-    }      
+	switch(opcao2) {
+		case 1:
+			wprintf(L"\nteste\n\n");
+			*op = lerInteiro("Deseja continuar no programa? Não/Sim", 0, 1);  // Asks if the user wants to coninue the program               
+			break;
+		case 0:
+			break;
+	}      
 }
 
 void menuAvarias(int *op){
-    int opcao2;
-    wprintf(L"\u2554\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2557\n");
-    wprintf(L"\u2551                Avarias                 \u2551\n");
-    wprintf(L"\u2560\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2563\n");
-    wprintf(L"\u2551  [1] Registar Avaria                   \u2551\n");
-    wprintf(L"\u2551  [2] Registar Reparação                \u2551\n");
-    wprintf(L"\u2551  [0] Anterior                          \u2551\n");
-    wprintf(L"\u255A\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u255D\n");
-    opcao2 = lerInteiro("Opcão", 0, 2);
+	int opcao2;
+	wprintf(L"╔══════════════════════════════════════════╗\n");
+	wprintf(L"║                Avarias                   ║\n");
+	wprintf(L"╠══════════════════════════════════════════╣\n");
+	wprintf(L"║  [1] Registar Avaria                     ║\n");
+	wprintf(L"║  [2] Registar Reparação                  ║\n");
+	wprintf(L"║  [0] Anterior                            ║\n");
+	wprintf(L"╚══════════════════════════════════════════╝\n");
+	opcao2 = lerInteiro("Opcão", 0, 2);
 
-    switch(opcao2){
-        case 1:
-            wprintf(L"\nteste\n\n");
-            *op = lerInteiro("Deseja continuar no programa? Não/Sim", 0, 1);  // Asks if the user wants to coninue the program               
-            break;
-        case 0:
-            break;
-    }
+	switch(opcao2){
+		case 1:
+			wprintf(L"\nteste\n\n");
+			*op = lerInteiro("Deseja continuar no programa? Não/Sim", 0, 1);  // Asks if the user wants to coninue the program               
+			break;
+		case 0:
+			break;
+	}
 }
 
 void menuDados(int *op){
